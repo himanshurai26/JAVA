0e1739abc05910d59f4356edb3897d499b974c8e
Himanshu <himanshukr98040@gmail.com>
2023-08-22 23:43:15 +0530

    generic

---
+++ com/generic/demo.java
@@ -0,0 +1,22 @@
+package com.generic;
+
+import java.util.*;
+
+public class demo {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+//      list.add(45);*/
+
+        List anotherlist = new ArrayList();
+        anotherlist.add(34);
+
+        list.add("Himanshu");
+        anotherlist.add("Himanshu");
+
+        System.out.println(list);
+        System.out.println(anotherlist);
+
+        }
+    }
+
